fd6e5ef3373067ea4f4c5f102e5fa87a4f629867
Jose Ostaiza <joseostaiza57@outlook.com>
2024-06-27 11:07:26 -0500

    update

--- src/android/LogicLinkPlugin.java
+++ src/android/LogicLinkPlugin.java
@@ -1,21 +1,19 @@
 package org.example.logiclinkplugin;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.webkit.MimeTypeMap;
 
-import org.apache.cordova.CordovaPlugin;
+import androidx.core.content.FileProvider;
+
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.content.Context;
-import android.net.Uri;
-import android.content.Intent;
-import androidx.core.content.FileProvider;
-import android.webkit.MimeTypeMap;
-import android.content.ActivityNotFoundException;
-import android.os.Build;
-import java.io.File;
 
-/**
- * This class starts an activity for an intent to view files
- */
+import java.io.File;
 public class LogicLinkPlugin extends CordovaPlugin {
 
 
@@ -23,67 +21,63 @@ public class LogicLinkPlugin extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("open")) {
             String path = args.getString(0);
-            this.chooseIntent(path, callbackContext);
+            this.openFile(path, callbackContext);
             return true;
         }
         return false;
     }
 
-    /**
-     * Returns the MIME type of the file.
-     *
-     * @param path
-     * @return
-     */
-    private static String getMimeType(String path) {
-        String mimeType = null;
-
-        String extension = MimeTypeMap.getFileExtensionFromUrl(path);
-        if (extension != null) {
-            MimeTypeMap mime = MimeTypeMap.getSingleton();
-            mimeType = mime.getMimeTypeFromExtension(extension.toLowerCase());
-        }
-
-        System.out.println("Mime type: " + mimeType);
-
-        return mimeType;
-    }
-
-    /**
-     * Creates an intent for the data of mime type
-     *
-     * @param path
-     * @param callbackContext
-     */
-    private void chooseIntent(String path, CallbackContext callbackContext) {
+  
+    private void openFile(String path, CallbackContext callbackContext) {
         if (path != null && path.length() > 0) {
             try {
                 Uri uri = Uri.parse(path);
                 String mime = getMimeType(path);
                 Intent fileIntent = new Intent(Intent.ACTION_VIEW);
 
-                // Set version to Android 12 (S) for example
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    Context context = cordova.getActivity().getApplicationContext();
-                    File file = new File(uri.getPath());
-                    Uri fileURI = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", file);
-                    fileIntent.setDataAndTypeAndNormalize(fileURI, mime);
-                    fileIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) { // API Level 16 -> Android 4.1
-                    fileIntent.setDataAndTypeAndNormalize(uri, mime);
+                Context context = cordova.getActivity().getApplicationContext();
+                File file = new File(uri.getPath());
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    // Use FileProvider to get content URI for file
+                    Uri fileUri = FileProvider.getUriForFile(context,
+                            context.getApplicationContext().getPackageName() + ".provider",
+                            file);
+                    fileIntent.setDataAndType(fileUri, mime);
+                    fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 } else {
                     fileIntent.setDataAndType(uri, mime);
                 }
 
-                cordova.getActivity().startActivity(fileIntent);
+                fileIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                callbackContext.success();
+                if (fileIntent.resolveActivity(context.getPackageManager()) != null) {
+                    context.startActivity(fileIntent);
+                    callbackContext.success();
+                } else {
+                    throw new ActivityNotFoundException();
+                }
+
             } catch (ActivityNotFoundException e) {
                 e.printStackTrace();
-                callbackContext.error(1);
+                callbackContext.error("Activity not found to handle this file type.");
+            } catch (Exception e) {
+                e.printStackTrace();
+                callbackContext.error("Error opening file: " + e.getMessage());
             }
         } else {
-            callbackContext.error(2);
+            callbackContext.error("File path is empty or null.");
         }
+    }
+
+    private String getMimeType(String path) {
+        String mimeType = null;
+
+        String extension = MimeTypeMap.getFileExtensionFromUrl(path);
+        if (extension != null) {
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
+        }
+
+        return mimeType;
     }
 }
